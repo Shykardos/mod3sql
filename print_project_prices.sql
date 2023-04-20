fed340c8c9eabe83e150f7786d7f90ad19e48099
Shykardos <127335784+Shykardos@users.noreply.github.com>
2023-04-20 21:29:57 +0300

    Create print_project_prices.sql

---
+++ print_project_prices.sql
@@ -0,0 +1,18 @@
+WITH project_durations AS (
+SELECT project.ID,
+EXTRACT(YEAR FROM AGE(project.FINISH_DATE, project.START_DATE)) * 12 +
+EXTRACT(MONTH FROM AGE(project.FINISH_DATE, project.START_DATE)) AS duration_in_months
+FROM project
+),
+project_costs AS (
+SELECT project.ID, SUM(worker.SALARY) * project_durations.duration_in_months AS cost
+FROM project
+JOIN project_durations ON project.ID = project_durations.ID
+JOIN project_worker ON project.ID = project_worker.PROJECT_ID
+JOIN worker ON project_worker.WORKER_ID = worker.ID
+GROUP BY project.ID, project_durations.duration_in_months
+)
+SELECT project.ID, project.CLIENT_ID, project.START_DATE, project.FINISH_DATE, project_costs.cost
+FROM project
+JOIN project_costs ON project.ID = project_costs.ID
+ORDER BY project_costs.cost DESC;
